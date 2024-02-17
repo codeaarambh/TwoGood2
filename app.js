3a26d638230b0af0ca3cdd280726421a0b920d61
codeaarambh <mrarav1234@gmail.com>
2024-02-18 00:10:18 +0530

    some pages added

--- app.js
+++ app.js
@@ -26,3 +26,50 @@ function apageAnimation() {
   });
 }
 apageAnimation();
+
+document.addEventListener("mousemove",function(a){
+  gsap.to(".cursor",{
+    left:a.x,
+    top:a.y
+  })
+})
+// document.querySelector("#child1").addEventListener("mouseenter", function() {
+//   gsap.to(".cursor", {
+//     transform: "translate(-50% ,-50%)scale(1)",
+    
+//   });
+// });
+// document.querySelector("#child1").addEventListener("mouseleave", function() {
+//   gsap.to(".cursor", {
+//     transform: "translate(-50% ,-50%)scale(0)"
+//   });
+// });
+
+// var a = document.querySelectorAll(".child");
+// a.forEach(function (elem) {
+//   elem.addEventListener("mouseenter", function () {
+//     gsap.to(".cursor", {
+//       transform: "translate(-50%, -50%) scale(1)",
+//     });
+//   });
+//   elem.addEventListener("mouseleave", function () {
+//     gsap.to(".cursor", {
+//       transform: "translate(-50%, -50%) scale(0)",
+//     });
+//   });
+// });
+var a = document.querySelectorAll(".child");
+var colors = ["#e5f0d6", " #f5dfcf", "#00000070", "pink"];
+a.forEach(function (elem, index) {
+  elem.addEventListener("mouseenter", function () {
+    gsap.to(".cursor", {
+      transform: "translate(-50%, -50%) scale(1)",
+      backgroundColor: colors[index],
+    });
+  });
+  elem.addEventListener("mouseleave", function () {
+    gsap.to(".cursor", {
+      transform: "translate(-50%, -50%) scale(0)",
+    });
+  });
+});
