f1efc0cda802ec37a8c83a7ca2a49f147fc9e3bf
codeaarambh <mrarav1234@gmail.com>
2024-02-18 11:14:39 +0530

    effects added

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
-function locoani(){
+function locoani() {
   gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".main"),
-  smooth: true,
-  smoothMobile: true,
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+    smoothMobile: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-});
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-
-
-
-
-
-
-
-
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
-// const scroll = new LocomotiveScroll({
-//   el: document.querySelector(".main"),
-//   smooth: true,
-//   smoothMobile: true,
-// });
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+  // const scroll = new LocomotiveScroll({
+  //   el: document.querySelector(".main"),
+  //   smooth: true,
+  //   smoothMobile: true,
+  // });
 }
-locoani()
+locoani();
 
-function navbarani(){
+function navbarani() {
   gsap.to(".nav-p1 img", {
     transform: "translateY(-100%)",
     scrollTrigger: {
@@ -55,23 +54,23 @@ function navbarani(){
       start: "top top",
       end: "top -5%",
       scrub: true,
-      ease:"linear"
-    }
+      ease: "linear",
+    },
   });
   gsap.to(".nav-p2 .links", {
     transform: "translateY(-100%)",
-    opacity:0,
+    opacity: 0,
     scrollTrigger: {
       trigger: ".page1",
       scroller: ".main",
       start: "top top",
       end: "top -5%",
       scrub: true,
-      ease:"linear"
-    }
+      ease: "linear",
+    },
   });
 }
-navbarani()
+navbarani();
 
 function apageAnimation() {
   let a = gsap.timeline();
@@ -93,6 +92,54 @@ function apageAnimation() {
   });
   a.from(".line", {
     opacity: 0,
+  });
+  gsap.from(".page3-textb", {
+    // y:100,
+   y:100,
+    opacity: 0,
+
+    scrollTrigger: {
+      trigger: ".page3-textb",
+      scroller: ".main",
+      // markers: true,
+ 
+      // scrub:true,
+    },
+    duration: 0.4,
+    stagger: 0.2,
+  });
+  gsap.from(".page2 .elem", {
+    // y:100,
+    scale: 0.7,
+    opacity: 0,
+
+    scrollTrigger: {
+      trigger: ".page2",
+      scroller: ".main",
+      // markers: true,
+      start: "top 30%",
+      // scrub:true,
+    },
+    duration: 0.4,
+    stagger: 0.4,
+  });
+
+
+
+  gsap.from(".page3 .child", {
+    // y:100,
+    scale: 0.3,
+    opacity: 0,
+
+    scrollTrigger: {
+      trigger: ".page3",
+      scroller: ".main",
+      // markers: true,
+      start: "top 60%",
+      // scrub:true,
+    },
+    duration: 0.2,
+    stagger: 0.3,
   });
 }
 apageAnimation();
